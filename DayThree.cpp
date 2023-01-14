149058d80570f9afd42f9dbad6dc9723ade28fca
shreyanshu07 <72155853+shreyanshu07@users.noreply.github.com>
2023-01-14 14:32:13 +0530

    Update DayThree.cpp

--- DayThree.cpp
+++ DayThree.cpp
@@ -1,10 +1,8 @@
 /******************************************************************************
-
 Welcome to GDB Online.
 GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
 C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
 Code, Compile, Run and Debug online from anywhere in world.
-
 *******************************************************************************/
 #include<bits/stdc++.h>
 using namespace std;
@@ -97,3 +95,216 @@ int sqrtBinarySearch(int n){
 }
 
 //Binary Search can be applied on monotonious functions only Eg : sqrt() is parabola
+
+
+/********************PRACTICE QUESTIONS*********************************/
+/*
+
+1. Order agnostic Binary Search
+2. 1st and last occurence of an Element
+3. Count of elements in a sorted array
+4. Number of Times the array is rotated
+5. Find an element in rotated array
+6. Searching in nearly sorted array
+7. Floor/Ceil of an Element
+8. Next Letter
+9. Index of last 1 int sorted array
+10. Find the position of an element in a sorted array
+11. Minimum difference element in a sorted array
+12. Bitonic array max element
+13. Search in bitonic array
+14. Search in rowWise + colWise sorted matrix
+15. Find element in sortedArray that appears only once
+16. Allocate minimum number of pages
+
+
+*/
+int binarySearchOrderNotKnown(int arr[],int n,int key) // Order agnostic Binary Search
+{
+	if(arr[0]<arr[n-1])
+	{
+		int start=0;
+		int end=n-1;
+		
+		while(start<=end)
+		{
+			int mid=start+(end-start)/2;
+			
+			if(arr[mid]==key)
+			{
+				return mid;
+			}
+			else if(arr[mid]>key)
+			{
+				end=mid-1;
+			}
+			else if(arr[mid]<key)
+			{
+				start=mid+1;
+			}
+		}
+         }
+         
+    else 
+	{
+		int start=0;
+		int end=n-1;
+		
+		while(start<=end)
+		{
+			int mid=start+(end-start)/2;
+			
+			if(arr[mid]==key)
+			{
+				return mid;
+			}
+			else if(arr[mid]>key)
+			{
+                start=mid+1;
+			}
+			else if(arr[mid]<key)
+			{
+				end=mid-1;
+			}
+		}
+         }
+         return -1;	
+}
+
+int searchInNearlySortedArray(int n,int arr[],int l,int h,int key){
+    while(l<=h){
+        int mid=l+(h-l)/2;
+        if(arr[mid]==key) return mid;
+        if((mid==0 or mid==n-1)) return -1;
+        if(arr[mid-1]==key) return mid-1;
+        if(arr[mid+1]==key) return mid+1;
+        if(arr[mid]<key) l=mid+1;
+        else if(arr[mid]>key) h=mid-1;
+    }
+    return -1;
+}
+
+int floorValueOfKey(int n,int arr[],int key){ // In this function element is returned instead of element index;
+    int l=0,h=n-1,res=-1;
+    while(l<=h){
+        int mid=l+(h-l)/2;
+        if(arr[mid]==key) return mid;
+        else if(arr[mid]<key){
+            res=arr[mid];
+            l=mid+1;
+        }
+        else h=mid-1;
+    }
+    return res;
+}
+
+int ceilValueOfKey(int n,int arr[],int key){ // In this function element is returned instead of element index;
+    int l=0,h=n-1,res=-1;
+    while(l<=h){
+        int mid=l+(h-l)/2;
+        if(arr[mid]==key) return mid;
+        else if(arr[mid]<key){
+            l=mid+1;
+        }
+        else{
+            
+            res=arr[mid];
+            h=mid-1;
+        }
+    }
+    return res;
+}
+
+/*QUES : Suppose you have a sorted array of infinite numbers, how would you search an element in the array?
+
+Since array is sorted, the first thing clicks into mind is binary search, but the problem here is that we don’t know size of array.
+If the array is infinite, that means we don’t have proper bounds to apply binary search. So in order to find position of key, first we find bounds and then apply binary search algorithm.
+
+Let low be pointing to 1st element and high pointing to 2nd element of array, Now compare key with high index element,
+-if it is greater than high index element then copy high index in low index and double the high index.
+-if it is smaller, then apply binary search on high and low indices found.*/
+
+int binarySearchInInfiniteArray(int arr[],int key){
+    int l=0,h=1;
+    while(arr[h]<key){
+        h*=2;
+    }
+    if(arr[h]==key) return mid;
+    return binarySearch(l,h,arr[],key);
+}
+
+int findPeakElement(vector<int> v){ //Finding the peak element
+    int n=v.size();
+    int l=0,r=n-1;
+    if(n==1) return 0;
+    if(n==2){
+        if(v[0]<v[1]) return 1;
+        else return 0;
+    }
+    if(v[0]>v[1]) return 0;
+    if(v[n-1]>v[n-2]) return n-1;
+    while(l<=r){
+        int mid=l+((r-l)/2);
+        cout<<mid<<" ";
+        if((mid==0 and v[mid]>v[mid+1]) or (mid==(n-1) and v[mid]>v[mid-1])) return mid;
+        else if(v[mid]>v[mid-1] and v[mid]>v[mid+1]) return mid;
+        else if(v[mid]<v[mid-1]) r=mid;
+        else if(v[mid]>v[mid-1]) l=mid;
+        
+    }
+    return -1;
+}
+
+/*
+ALLOCATE MINIMUM NUMBER OF PAGES:
+
+Given number of pages in n different books and m students. The books are arranged in ascending order of number of pages. Every student is assigned to read some consecutive books. The task is to assign books in such a way that the maximum number of pages assigned to a student is minimum.
+
+Example :
+
+Input : pages[] = {12, 34, 67, 90}
+        m = 2
+Output : 113
+Explanation:
+There are 2 number of students. Books can be distributed 
+in following fashion : 
+  1) [12] and [34, 67, 90]
+      Max number of pages is allocated to student 
+      2 with 34 + 67 + 90 = 191 pages
+  2) [12, 34] and [67, 90]
+      Max number of pages is allocated to student
+      2 with 67 + 90 = 157 pages 
+  3) [12, 34, 67] and [90]
+      Max number of pages is allocated to student 
+      1 with 12 + 34 + 67 = 113 pages
+
+Of the 3 cases, Option 3 has the minimum pages = 113. 
+*/
+
+int isPossible(int A[],int n,long long sum,long long tot){
+    long long res=0,i=0,ans=0;
+    while(i<n){
+        res=0;
+        while(i<n and (A[i]+res<=sum)){
+            res+=A[i];
+            i++;
+        }
+        ans++;
+    }
+    return ans;
+}
+int findPages(int a[], int n, int M) 
+{
+    //code here
+    int mini=INT_MAX;long long sum=0;
+    for(int i=0;i<n;i++) {mini=min(mini,a[i]);sum+=a[i];}
+    long long l=mini,h=sum-mini,ans=0;
+    while(l<=h){
+        long long m=l+((h-l)/2);
+        int num=isPossible(a,n,m,sum);
+        if(num>M) l=m+1;
+        else if(num<=M) {h=m-1;ans=m;}
+        
+    }
+    return ans;
+}
